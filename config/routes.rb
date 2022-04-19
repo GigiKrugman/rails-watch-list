02f77a095b9e4b9e470bc44266fd178556cbc464
Marcello Mulas <marcellomulas93@gmail.com>
2022-04-19 19:48:31 +0200

    Second Steps

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
+  end
+  resurces :bookmarks, only: :destroy
+  resurces :reviews, only: :destroy
 end
